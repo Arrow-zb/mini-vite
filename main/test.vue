6f9690f2d3906c2879756a2e2eb571a15e1f371d
arrow <arrow_zb@outlook.com>
2021-03-01 15:39:20 +0800

    feat: vue sfc

---
+++ main/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <button @click="add">count: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+
+  methods: {
+    add() {
+      this.count += 1;
+    }
+  }
+}
+</script>
